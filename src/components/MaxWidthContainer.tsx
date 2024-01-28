0d2b16a921f8dc334fecc1082700e49e866416c4
Barnakrum <krzysiu03@tlen.pl>
2024-01-28 21:26:42 +0100

    Initial commit

---
+++ src/components/MaxWidthContainer.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import React, { ReactNode } from "react";
+
+const MaxWidthContainer = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto flex w-full sm:max-w-[90%] md:max-w-[85%] xl:max-w-[80%]",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthContainer;
