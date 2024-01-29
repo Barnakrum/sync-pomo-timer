d0f174b7db6f0794ca50d138ac22ff0c19cefc13
Barnakrum <krzysiu03@tlen.pl>
2024-01-29 13:46:04 +0100

    Initial commit

---
+++ src/components/ThemePicker.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { Button, buttonVariants } from "./ui/button";
+
+const ThemePicker = () => {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="p-2 hover:bg-inherit">
+          <SunIcon className=" scale-100 dark:scale-0" />
+          <MoonIcon className="absolute scale-0 transition-all dark:scale-100" />
+
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ThemePicker;
